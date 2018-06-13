1252b9656ac08e5ae9563fb62385372e5142da6f
Vitor Paixão <vitor@vitorpaixao.com>
2018-06-13 02:06:14 -0300

    Alterações

--- dist/js/app.js
+++ dist/js/app.js
@@ -16595,7 +16595,7 @@ $(document).ready(function () {
         $('#content .codeCollapse').addClass('closed');
         
       }
-      if ( size >= 800) {
+      if ( size >= 769) {
         console.log('bigger 800');
         $('#sidebar').removeClass('closed');
         $('#content').removeClass('closed');
